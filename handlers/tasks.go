4e619fcaf032a344bfbd37cca8b699dffe7a8c7d
TiaWidi <tia.septianawidi@gmail.com>
2025-04-09 17:49:57 +0700

    Define the Task Model

---
+++ handlers/tasks.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"time"
+	"gorm.io/gorm"
+)
+
+type Task struct {
+	ID 			uint 		`json:"id" gorm:"primayKey"`
+	Title 		string 		`json:"title`
+	Description string 		`json:"description"`
+	Status 		string 		`json:"status"`
+	CreatedAt 	time.Time 	`json:"created_at"`
+	UpdatedAt 	time.Time 	`json:"updated_at"`
+}
